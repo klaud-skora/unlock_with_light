1103db2d668e6021cf6fd386d82220b0600527bf
Klaudia Skóra <klaud.skora@gmail.com>
2020-05-27 13:15:24 +0200

    Use _luxController as broadcast and add _contentController for displaying proper widget regarding the unlocker.state

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -16,9 +16,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   static int pin = 1524;
   Unlocker unlocker = Unlocker(pin);
-  final StreamController<int> _luxController = StreamController<int>();
+  final StreamController<int> _luxController = StreamController<int>.broadcast();
+  final StreamController _contentController = StreamController();
 
-  int _lux;
   Light _light = new Light();
   StreamSubscription _subscription;
 
@@ -35,6 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void onData(int luxValue) async {
     _luxController.sink.add(luxValue);
+    _contentController.sink.add(unlocker.state);
   }
 
    void stopListening() {
@@ -52,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
     try {
       stopListening();
        _luxController.close();
+       _contentController.close();
     } catch (exception) {
       print(exception.toString());
     } finally {
@@ -73,107 +75,134 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         color: Colors.purple.withOpacity(.2),
         child: Center(
-          child: unlocker.state is UnlockingStatus ? Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 50.0),
-              Text('Use lux value to tap the pin code.'),
-              SizedBox(height: 10.0),
-              Text('Touch each of four cards to set it with a running value.'),
-              SizedBox(height: 150.0),
-              Row(    
+          child: StreamBuilder(
+            stream: _contentController.stream,
+            initialData: UnlockingStatus,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return  snapshot.data is UnlockingStatus ? Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Container(
-                    width: 50.0,
-                    height: 66.0,
-                    child: RaisedButton(
-                      color: Colors.purple.withOpacity(.1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.purple.shade900),
+                  SizedBox(height: 50.0),
+                  Text('Use lux value to tap the pin code.'),
+                  SizedBox(height: 10.0),
+                  Text('Touch each of four cards to set it with a running value.'),
+                  SizedBox(height: 150.0),
+                  Row(    
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 50.0,
+                        height: 66.0,
+                        child: StreamBuilder( 
+                          stream: _luxController.stream,
+                          initialData: null,
+                          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                            return RaisedButton(
+                              color: Colors.purple.withOpacity(.1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.purple.shade900)
+                              ),
+                              onPressed: () => { unlocker.setCard(Cards.one, snapshot.data) },
+                              child: Text(unlocker.values[3] == Value.blank ? '?' : '*'),
+                            );
+                          },
+                        ),
                       ),
-                      onPressed: () => { unlocker.setCard(Cards.one, _lux) },
-                      child: Text(unlocker.values[0] == Value.blank ? '?' : '*'),   
-                    ),
+                      SizedBox(width: 10.0),
+                      Container(
+                        width: 50.0,
+                        height: 66.0,
+                        child: StreamBuilder( 
+                          stream: _luxController.stream,
+                          initialData: null,
+                          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                            return RaisedButton(
+                              color: Colors.purple.withOpacity(.1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.purple.shade900)
+                              ),
+                              onPressed: () => { unlocker.setCard(Cards.two, snapshot.data) },
+                              child: Text(unlocker.values[3] == Value.blank ? '?' : '*'),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10.0),
+                      Container(
+                        width: 50.0,
+                        height: 66.0,
+                        child: StreamBuilder( 
+                          stream: _luxController.stream,
+                          initialData: null,
+                          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                            return RaisedButton(
+                              color: Colors.purple.withOpacity(.1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.purple.shade900)
+                              ),
+                              onPressed: () => { unlocker.setCard(Cards.three, snapshot.data) },
+                              child: Text(unlocker.values[3] == Value.blank ? '?' : '*'),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10.0),
+                      Container(
+                        width: 50.0,
+                        height: 66.0,
+                        child: StreamBuilder( 
+                          stream: _luxController.stream,
+                          initialData: null,
+                          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                            return RaisedButton(
+                              color: Colors.purple.withOpacity(.1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.purple.shade900)
+                              ),
+                              onPressed: () => { unlocker.setCard(Cards.four, snapshot.data) },
+                              child: Text(unlocker.values[3] == Value.blank ? '?' : '*'),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(width: 10.0),
-                  Container(
-                    width: 50.0,
-                    height: 66.0,
-                    child: RaisedButton(
-                      color: Colors.purple.withOpacity(.1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.purple.shade900),
-                      ),
-                      onPressed: () => { unlocker.setCard(Cards.two, _lux) },
-                      child: Text(unlocker.values[1] == Value.blank ? '?' : '*'),
-                    ),
+                  SizedBox(height: 50.0),
+                  Text('Present value:', style: TextStyle( fontSize: 22.0 )),
+                  SizedBox(height: 10.0),
+                  StreamBuilder(
+                    stream: _luxController.stream,
+                    builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                      return Text( snapshot.data == null ? '(no value was found)' : '${snapshot.data}', 
+                        style: TextStyle( fontSize: 22.0 ),
+                      );
+                    }
                   ),
-                  SizedBox(width: 10.0),
-                  Container(
-                    width: 50.0,
-                    height: 66.0,
-                    child: RaisedButton(
-                      color: Colors.purple.withOpacity(.1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.purple.shade900),
-                      ),
-                      onPressed: () => { unlocker.setCard(Cards.three, _lux) },
-                      child: Text(unlocker.values[2] == Value.blank ? '?' : '*'),
-                    ),
-                  ),
-                  SizedBox(width: 10.0),
-                  Container(
-                    width: 50.0,
-                    height: 66.0,
-                    child: RaisedButton(
-                      color: Colors.purple.withOpacity(.1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.purple.shade900)
-                      ),
-                      onPressed: () => { unlocker.setCard(Cards.four, _lux) },
-                      child: Text(unlocker.values[3] == Value.blank ? '?' : '*'),
-                    ),
+                  SizedBox(height: 70.0),
+                ],
+              ) : unlocker.verifier == Verifier.correct ? 
+              /* PIN IS CORRECT */
+              Text('You successfully unlocked the app!') 
+              /* PIN IS INCORRECT */
+              : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('The pin is WRONG', style: TextStyle(fontWeight: FontWeight.w500)),
+                  SizedBox(height: 20.0),
+                  FlatButton(
+                    shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(12.0) ),
+                    color: Colors.purple,
+                    onPressed: () => unlocker.reset(),
+                    child: Text('Try again', style: TextStyle(color: Colors.white)),
                   ),
                 ],
-              ),
-              SizedBox(height: 50.0),
-              Text('Present value:', style: TextStyle( fontSize: 22.0 )),
-              SizedBox(height: 10.0),
-              // Text(_lux == -1 ? '(no value was found)' : '$_lux', 
-              //   style: TextStyle( fontSize: 22.0 ),
-              // ),
-              StreamBuilder(
-                stream: _luxController.stream,
-                builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-                  return Text( snapshot.data == null ? '(no value was found)' : '${snapshot.data}', 
-                    style: TextStyle( fontSize: 22.0 ),
-                  );
-                }
-              ),
-              SizedBox(height: 70.0),
-            ],
-          ) : unlocker.verifier == Verifier.correct ? 
-          /* PIN IS CORRECT */
-          Text('You successfully unlocked the app!') 
-          /* PIN IS INCORRECT */
-          : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('The pin is WRONG', style: TextStyle(fontWeight: FontWeight.w500)),
-              SizedBox(height: 20.0),
-              FlatButton(
-                shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(12.0) ),
-                color: Colors.purple,
-                onPressed: () => unlocker.reset(),
-                child: Text('Try again', style: TextStyle(color: Colors.white)),
-              ),
-            ],
-          ), 
+              ); 
+            }
+          ),
         ),
       ),
     );
